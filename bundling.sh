a15666f12969f4db175a18d231f3630a20dcfff9
Fabian Prasser <mail@fabian-prasser.de>
2024-04-09 17:06:25 +0200

    Fix typos

--- bundling.sh
+++ bundling.sh
@@ -4,19 +4,19 @@ echo "Installing Webpack and dependencies"
 npm install --save-dev webpack webpack-cli
 npm install --save-dev html-loader css-loader style-loader html-webpack-plugin mini-css-extract-plugin
 
-echo "Bunding GHDM Data"
+echo "Bundling GHDM Data"
 cd ./ghdm-data/
 npx webpack
 
-echo "Bunding GHDM UI"
+echo "Bundling GHDM UI"
 cd ../ghdm-ui/
 npx webpack
 
-echo "Bunding Herald Core"
+echo "Bundling Herald Core"
 cd ../herald-core/
 npx webpack
 
-echo "Bunding Herald UI"
+echo "Bundling Herald UI"
 cd ../herald-ui/
 npx webpack
 
